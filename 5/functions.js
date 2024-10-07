6fe02ae9d5b236a993098816ecf745ee2d1adf23
Nik125Y <topnikm@gmail.com>
2024-10-07 12:11:17 +0300

    Update functions.js

--- 5/functions.js
+++ 5/functions.js
@@ -2,13 +2,14 @@ function click1(event) {
   let f1 = document.getElementsByName("field1");
   let r = document.getElementById("result");
   let s = document.getElementsByName("select1");
+  let m = f1.value;
+  if (m.match(/^\d+$/) !== null) {
   if (s[0].value == "v1") r.innerHTML = f1[0].value * 320;
   if (s[0].value == "v2") r.innerHTML = f1[0].value * 2;
   if (s[0].value == "v3") r.innerHTML = f1[0].value * 1100;
   if (s[0].value == "v4") r.innerHTML = f1[0].value * 210;
-  
-  let m = f1.value;
-  if (m.match(/^\d+$/) === null) r.innerText ="некорректное значение";
+  }
+  else r.innerText ="некорректное значение";
   
   return false;
   event.preventDefault();
