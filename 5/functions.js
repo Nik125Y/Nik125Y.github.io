2a72227a20f5fe5798db14db6afbe9defe77b1a4
Nik125Y <topnikm@gmail.com>
2024-10-07 12:02:57 +0300

    Update functions.js

--- 5/functions.js
+++ 5/functions.js
@@ -7,9 +7,8 @@ function click1(event) {
   if (s[0].value == "v3") r.innerHTML = f1[0].value * 1100;
   if (s[0].value == "v4") r.innerHTML = f1[0].value * 210;
   
-  /*let m = r.match(/[0123456789]/gi);
-  if (m === null)  r.innerHTML ="некорректное значение";*/
-  
+  let m = f1[0].value.match(/^\d+$/);
+  if (m === null) r.innerHTML ="некорректное значение";*
   
   return false;
   event.preventDefault();
