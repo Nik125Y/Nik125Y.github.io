adad8a35d3cfcefa671937cbdec9aae6434b6176
Nik125Y <topnikm@gmail.com>
2024-10-13 20:12:41 +0300

    Update functions.js

--- 6/functions.js
+++ 6/functions.js
@@ -3,7 +3,7 @@ function click1(event) {
     let r = document.getElementById("result");
     let s = document.getElementsByName("select1");
     
-    s[0].addEventListener("change", function(event) {
+    /*s[0].addEventListener("change", function(event) {
     let select = event.target;
     let radios = document.getElementsByClassName("radiobutton");
     if (select.value == "v3") {
@@ -18,7 +18,7 @@ function click1(event) {
     r1.forEach(function(radio) {
     radio.addEventListener("change", function(event) {
     let r1 = event.target;
-    });
+    });*/
         
     let m = f1[0].value;
     if (s[0].value === "v1") {
