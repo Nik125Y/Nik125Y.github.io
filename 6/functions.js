1142a7883bdf29f3fc9e6482533e03e2ae5992f7
Nik125Y <topnikm@gmail.com>
2024-10-13 22:01:38 +0300

    Update functions.js

--- 6/functions.js
+++ 6/functions.js
@@ -1,9 +1,7 @@
-window.addEventListener('DOMContentLoaded', function () {
+function click1(event) {
     let s = document.getElementsByName("select1");
     let f1 = document.getElementsByName("field1");
     let r = document.getElementById("result");
-    
-    function click1(event) {
     let m = f1[0].value;
     if (s[0].value === "v1") {
         r.innerHTML = f1[0].value * 23200;
@@ -17,28 +15,32 @@ window.addEventListener('DOMContentLoaded', function () {
     if (m.match(/^\d+$/) === null) {
         r.innerHTML = "некорректное значение";
     }
+    event.preventDefault();
+    return false;
 }
-
-
-    
-  /*  s[0].addEventListener("change", function() {
+window.addEventListener("DOMContentLoaded", function () {
+    let b = document.getElementById("button1");
+    b.addEventListener("click", click1);
+});
+window.addEventListener('DOMContentLoaded', function (event) {
+  let s = document.getElementsByName("select1");
+  s[0].addEventListener("change", function(event) {
     let select = event.target;
     let radios = document.getElementById("radiobutton");
-    console.log(select.value);            //remove
-    if (select.value === "v3") {
+    console.log(select.value);
+    if (select.value == "v3") {
       radios.style.display = "none";
     }
     else {
       radios.style.display = "block";
     }
-  });*/
-   f1.addEventListener("input", function () {
-        click1();
-    });
-  let rad = document.getElementsByName("radiobutton");
-  rad.forEach(function(radio) {
-    radio.addEventListener("change", function() {
-        click1();
+  });
+  
+  let r = document.querySelectorAll(".radiobutton input[type=radio]");
+  r.forEach(function(radio) {
+    radio.addEventListener("change", function(event) {
+      let r = event.target;
+      console.log(r.value);
     });    
   });
   
