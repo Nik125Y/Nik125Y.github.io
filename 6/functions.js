224167199a3935129da19094ade54a846b20c5c5
Nik125Y <topnikm@gmail.com>
2024-10-14 14:54:42 +0300

    Update functions.js

--- 6/functions.js
+++ 6/functions.js
@@ -6,8 +6,14 @@ window.addEventListener('DOMContentLoaded', function () {
     radio.addEventListener("change", function(event) {
       let r = event.target;
         console.log(r.value);
-      
-        if (r.value === "r1" || r.value === "r2") {
+      if (r.value === "r1" ) {
+      chek.style.display = "none";
+      s.style.display = "none";
+          }
+          else {s.style.display = "block";
+    chek.style.display = "block";
+      }
+       /* if (r.value === "r1" || r.value === "r2") {
       chek.style.display = "none";
           if (r.value === "r1") {
             s.style.display = "none";
@@ -23,7 +29,7 @@ window.addEventListener('DOMContentLoaded', function () {
       else {
         s.style.display = "block";
       }
-     
+     */
     });    
   });
   
@@ -31,7 +37,7 @@ window.addEventListener('DOMContentLoaded', function () {
 function click1(event) {
     let r1 = document.querySelectorAll(".radiobutton input[type=radio]");
     let f1 = document.getElementsByName("field1");
-    let s = document.getElementById("sel");
+    let s = document.getElementsByName("select1");
     let r = document.getElementById("result");
     let c = document.getElementsByName("check");
     let m = f1[0].value;
@@ -49,9 +55,9 @@ function click1(event) {
     }*/
     if (r1[2].checked) {
       r.innerHTML = f1[0].value*1100;
-      if (c.value === "c1")
+      if (c[0].value === "c1")
         r.innerHTML += 100; 
-      if (c.value === "c1")
+      if (c[0].value === "c2)
         r.innerHTML += 500; 
     }
     if (m.match(/^\d+$/) === null) {
