2acbd2ec0444424d1008f7773d5c82cfe1975036
Nik125Y <topnikm@gmail.com>
2024-10-14 16:50:24 +0300

    Update listing.js

--- 7/listing.js
+++ 7/listing.js
@@ -1,16 +1,5 @@
-/*jslint browser: true*/ /*global  $*/
-$(document).ready(function() {
-    $(".carousel").slick({
-        dots: true,
-        infinite: true,
-        responsive: [
-            {
-            breakpoint: 1024,
-            settings: {
-            slidesToShow: 1
-            }
-            }
-        ],
-        slidesToShow: 3
-    });
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
 });
