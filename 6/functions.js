7be7c91b5d51c4588a9ec3d5e2b27d38c02b9401
Nik125Y <topnikm@gmail.com>
2024-10-14 11:42:08 +0300

    Update functions.js

--- 6/functions.js
+++ 6/functions.js
@@ -1,26 +1,31 @@
 function click1(event) {
-    //let s = document.getElementById("radiobutton");
+    let select = document.getElementsByName("selest1");
+    let chek = document.getElementById("check");
     let r1 = document.querySelectorAll(".radiobutton input[type=radio]");
     let f1 = document.getElementsByName("field1");
     let r = document.getElementById("result");
     let m = f1[0].value;
-    console.log(r1.value);
-    r1.forEach(function(radio) {
-    radio.addEventListener("change", function(event) {
-      let r1 = event.target;
-       if (r1[0].value === "r1") {
-        r.innerHTML = f1[0].value * 23200;
+    console.log(r1[0].value);
+    
+       if (r1[0].checked) {
+        r.innerHTML = f1[0].value * 23200; 
+       select[0].style.display = "block";
+      chek.style.display = "block";
     }
-    if (r1[1].value === "r2") {
+    else {
+    select[0].style.display = "none";
+      chek.style.display = "none";
+    }
+    if (r1[1].checked) {
         r.innerHTML = f1[0].value * 1;
     }
-    if (r1[2].value === "r3") {
+    if (r1[2].checked) {
         r.innerHTML = f1[0].value * 1100;
     }
     if (m.match(/^\d+$/) === null) {
         r.innerHTML = "некорректное значение";
     }
-    });   
+    
     
     return false;
 }
